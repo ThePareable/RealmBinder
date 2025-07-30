77e261e1090a2edf217bfe6c7438f4ec9072e5a4
ThePareable <bugramuhcii@gmail.com>
2025-07-30 13:59:54 +0300

    first commit

---
+++ realm-binder/src/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>RealmBinder'a Hoşgeldiniz!</h1>
+    <p>DND dünyanızı yönetmeye başlamak için giriş yapın veya kayıt olun.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
